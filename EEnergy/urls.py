8d308c18a523e2051c78cbd809f2e6dffd836989
Ahmed Radwan <ahmedradwan9966@gmail.com>
2020-04-02 12:21:10 +0200

    add api/v1/ to the energy_resources app url

--- EEnergy/urls.py
+++ EEnergy/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('energy_resources.urls'))
+    path('api/v1/',include('energy_resources.urls'))
 ]
