841ea062e57d47183584478a461a79ed1c223f6a
Ahmed Radwan <ahmedradwan9966@gmail.com>
2020-04-02 12:22:08 +0200

    create EnergyResource and User Serializer

---
+++ energy_resources/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import EnergyResource
+#from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+
+class EnergyResourceSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = EnergyResource
+        fields = ['id', 'resource_type',
+                  'status', 'capacity', 'owner']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    energy_resources = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=EnergyResource.objects.all())
+
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'energy_resources']
