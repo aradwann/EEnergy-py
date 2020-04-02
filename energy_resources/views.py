bb1218c33a2b8d6e90eabff82732e914867ff6dc
Ahmed Radwan <ahmedradwan9966@gmail.com>
2020-04-02 12:23:34 +0200

    wr detail & list views for EnergyResource and User

--- energy_resources/views.py
+++ energy_resources/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from .serializers import EnergyResourceSerializer, UserSerializer
+from rest_framework import generics
+from .models import EnergyResource
+from django.contrib.auth import get_user_model 
 
-# Create your views here.
+
+class EnergyResourcesList(generics.ListCreateAPIView):
+    queryset = EnergyResource.objects.all()
+    serializer_class = EnergyResourceSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class EnergyResourceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = EnergyResource.objects.all()
+    serializer_class = EnergyResourceSerializer
+
+
+
+class UsersList(generics.ListAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
