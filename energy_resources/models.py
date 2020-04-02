21f58f3d4ee7b44dab872715bda72bc717031228
Ahmed Radwan <ahmedradwan9966@gmail.com>
2020-04-02 11:36:52 +0200

    create EnergyResource model

--- energy_resources/models.py
+++ energy_resources/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class EnergyResource(models.Model):
+    resource_type = models.CharField(max_length=200)
+    status = models.CharField(max_length=40)
+    capacity = models.FloatField()
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
