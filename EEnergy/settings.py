8b616cdff66260371fa863ba110ba09ea8336e7f
Ahmed Radwan <ahmedradwan9966@gmail.com>
2020-04-02 13:16:07 +0200

    use allauth registration, token authentication

--- EEnergy/settings.py
+++ EEnergy/settings.py
@@ -37,9 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd party
     'rest_framework',
+    'rest_framework.authtoken',
+    'allauth', 
+    'allauth.account', 
+    'allauth.socialaccount', 
+    'rest_auth',
+    'rest_auth.registration',
 
     # local
     'energy_resources',
@@ -128,5 +135,12 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ], 'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+
+SITE_ID = 1
