8229d82f777e48975fdba31c433cfb9b26b367e2
Ahmed Radwan <ahmedradwan9966@gmail.com>
2020-04-15 21:00:23 +0000

    generating token with creating new user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+
+from rest_framework.authtoken.models import Token
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+@receiver(post_save, sender=get_user_model())
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
