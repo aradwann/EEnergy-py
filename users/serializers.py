cb01e0068b57254bd901a11b1fb1ac6a35dc8cd1
Ahmed Elmaghallawy <ahmedradwan9966@gmail.com>
2020-05-22 02:33:27 +0000

    add email field to serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,10 @@
 from rest_framework import serializers
-from energy_resources.models import EnergyResource
 from django.contrib.auth import get_user_model
 
 
 class UserSerializer(serializers.ModelSerializer):
-    energy_resources = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=EnergyResource.objects.all())
+    """serialize user instance"""
 
     class Meta:
         model = get_user_model()
-        fields = ['id', 'username', 'energy_resources']
+        fields = ['id', 'username', 'email']
