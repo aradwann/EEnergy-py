ba57dba2b90ee394ee39d92283867c63c53912c3
Ahmed Elmaghallawy <ahmedradwan9966@gmail.com>
2020-05-28 11:19:54 +0200

    add nearby serializer

--- energy_resources/serializers.py
+++ energy_resources/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import EnergyResource
+from users.serializers import UserSerializer
 
 
 class EnergyResourceSerializer(serializers.ModelSerializer):
-    """Model serializer for energy resource instance"""
+    """list serializer for energy resource instances"""
     owner = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
@@ -13,3 +14,19 @@ class EnergyResourceSerializer(serializers.ModelSerializer):
                   'capacity', 'consumption',
                   'net_energy', 'owner']
         read_only_fields = ['id', 'owner', 'net_energy']
+
+
+class EnergyResourceDetailSerializer(EnergyResourceSerializer):
+    """detail serializer for energy resource instance"""
+    owner = UserSerializer()
+
+
+class NearbyEnergyResourceSerializer(serializers.ModelSerializer):
+    """nearby energy resource serializer for energy resource instance"""
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)
+    distance = serializers.FloatField()
+
+    class Meta:
+        model = EnergyResource
+        fields = ['id', 'net_energy', 'owner', 'distance']
+        read_only_fields = ['id', 'owner', 'net_energy', 'distance']
